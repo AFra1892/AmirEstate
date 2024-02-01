089af5dc2172605b5a0444064c4f7daa6928ad6c
amir <mashhadifarahani1@gmail.com>
2024-02-02 00:27:35 +0330

    end of step 8

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
+dotenv.config()
+
+
+
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+    console.log('connected to db');
+}).catch((err)=>{console.log(err);})
+const app = express();
+app.listen(3000,()=>{
+    console.log('server is running on port 3000...');
+})
+
+app.use("/api/user",userRouter)
