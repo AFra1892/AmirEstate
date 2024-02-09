2c3bfdb058cd551c738b876db1c7331d04ac6503
amir <mashhadifarahani1@gmail.com>
2024-02-09 14:12:17 +0330

    add google sign in but it doesnt work

---
+++ client/src/firebase.js
@@ -0,0 +1,13 @@
+//this method doesnt work
+
+import {initializeApp} from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey:"AIzaSyBm4H2OeXE72Jsy2upvQvO8ErU7qQjP7c4",
+    authDomain:"mern-80dc0.firebaseapp.com",
+    projectId:"mern-80dc0",
+    storageBucket:"mern-80dc0.appspot.com",
+    messagingSenderId:"779130904386",
+    appId:"1:779130904386:web:16d4521600dfa9c268b2db"
+}
+export const app = initializeApp(firebaseConfig);
